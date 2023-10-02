71de08020ecbb147bc4939fca5a162891749959b
basma11elmihy <basma.elmihy@gmail.com>
2023-10-02 19:06:30 +0300

    fixed fetch user

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,12 +1,14 @@
 import AccountProfile from "@/components/forms/AccountProfile";
-
+import { fetchUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 import { currentUser } from "@clerk/nextjs";
 
 async function Page(){
     const user = await currentUser();
-    const userInfo = {
-    
-    };
+    if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect("/");
     const userData = {
         id: user?.id,
         objectId: userInfo?._id,
